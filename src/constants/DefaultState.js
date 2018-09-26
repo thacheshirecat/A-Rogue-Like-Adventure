c23cd25d8b364d170dd83f3948b775c05db95640
Guest User <Guest@epicodus-55.local>
2018-09-26 08:56:20 -0700

    Add a default state file to the constants directory, including first, incomplete event data object.

---
+++ src/constants/DefaultState.js
@@ -0,0 +1,9 @@
+export const EventData = {
+  '1' : {
+    eventName: 'Begin Again',
+    eventType: 'dialogue',
+    optionOneText: 'Head West',
+    optionTwoText: 'Head East',
+    optionThreeText: 'Head North'
+  }
+};
